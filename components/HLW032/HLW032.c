1ecf320883de68fa2270474bc023326bf45f3c12
Fairy <2754283833@qq.com>
2023-02-16 22:39:33 +0800

    新建HLW8032组件文件夹

---
+++ components/HLW032/HLW032.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "HLW032.h"
+
+void func(void)
+{
+
+}
--- main/main.c
+++ main/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 #include "BH1750.h"
 #include "SHTC3.h"
+
+#define EXAMPLE_ESP_WIFI_SSID "llx"
+#define EXAMPLE_ESP_WIFI_PASS "12345678"
+#define EXAMPLE_ESP_WIFI_CHANNEL 6
+#define EXAMPLE_MAX_STA_CONN 4
+
+static const char *TAG = "wifi softAP";
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+void wifi_init_softap(void);
 
 void app_main(void)
 {
@@ -61,6 +80,18 @@ void app_main(void)
     bh1750_cnt_meas(BH1750_INS_CNT_H1_MOD);
     vTaskDelay(200 / portTICK_PERIOD_MS);
 
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+
     while (1)
     {
         shtc3_measure_normal_rh_en_clocks(Humidity, Temperature);
@@ -95,7 +126,7 @@ void app_main(void)
         bh1750_cnt_meas(BH1750_INS_CNT_H1_MOD);
         vTaskDelay(200 / portTICK_PERIOD_MS);
         bh1750_read_data(&light);
-        printf("ligth = %d lx\t", light);
+        printf("ligth = %d lm\t", light);
         vTaskDelay(700 / portTICK_PERIOD_MS);
     }
 
@@ -109,3 +140,75 @@ void app_main(void)
     // fflush(stdout);
     // esp_restart();
 }
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED)
+    {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
+    {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init_softap(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+            .pmf_cfg = {
+                .required = false,
+            },
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0)
+    {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+}
+
+// void app_main(void)
+// {
+//     // Initialize NVS
+//     esp_err_t ret = nvs_flash_init();
+//     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+//     {
+//         ESP_ERROR_CHECK(nvs_flash_erase());
+//         ret = nvs_flash_init();
+//     }
+//     ESP_ERROR_CHECK(ret);
+
+//     ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+//     wifi_init_softap();
+// }
