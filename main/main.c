cfbce62393562aad1c2b047d5cc2aed2b15493d4
Fairy <2754283833@qq.com>
2023-03-12 18:10:23 +0800

    Update main.c
    MQTT测试代码

--- main/main.c
+++ main/main.c
@@ -28,31 +28,65 @@
 #include "HLW032.h"
 #include "BasicDrive.h"
 
-#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
-
-#define EXAMPLE_ESP_WIFI_SSID "fairy"
-#define EXAMPLE_ESP_WIFI_PASS "12345678"
-#define EXAMPLE_ESP_MAXIMUM_RETRY 10
-
-static int s_retry_num = 0;
-
-static const char *TAG = "wifi sta";
-
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+
+#define EXAMPLE_ESP_WIFI_SSID "fairy"
+#define EXAMPLE_ESP_WIFI_PASS "12345678"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 10
+
+// #define CONFIG_BROKER_URL "mqtt://mqtt.eclipseprojects.io"
+#define BROKER_URL "iot-06z00fj6casse3y.mqtt.iothub.aliyuncs.com"
+
+#define MQTT_USER_NAME "4JxiKytRv9OkNx1aB7yX&a14U3nfkMWp"
+#define MQTT_PASSWORD "15adf4434872cb42ec510f5d516cc4d9a1b1ec23cd54ed98f4490817e882ed85"
+#define MQTT_HOSTNAME "a14U3nfkMWp.iot-as-mqtt.cn-shanghai.aliyuncs.com"
+#define MQTT_PORT 1883
+#define MQTT_CLIENT_ID "a14U3nfkMWp.4JxiKytRv9OkNx1aB7yX|securemode=2,signmethod=hmacsha256,timestamp=1678615001639|"
+#define MQTT_DVEICE_NAME "4JxiKytRv9OkNx1aB7yX"
+
+// #define MQTT_USER_NAME "SmartScoket02&a1mDa96Wei7"
+// #define MQTT_PASSWORD "ae144a7141e341f21eb8937eba211042157a9cea6a02647d68d7c1b8351e2507"
+// #define MQTT_HOSTNAME "a1mDa96Wei7.iot-as-mqtt.cn-shanghai.aliyuncs.com"
+// #define MQTT_PORT 1883
+// #define MQTT_CLIENT_ID "a1mDa96Wei7.SmartScoket02|securemode=2,signmethod=hmacsha256,timestamp=1678613495121|"
+// #define MQTT_DVEICE_NAME "SmartScoket02"
+
+static int s_retry_num = 0;
+
+static const char *TAG = "wifi sta";
+static const char *MQTT_TAG = "MQTT";
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
 static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
 void wifi_init_sta(void);
+static void mqtt_app_start(void);
+static void log_error_if_nonzero(const char *message, int error_code);
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 
 void app_main(void)
 {
-
     xTaskCreate(Task_key, "Task_key", 2048, NULL, 3, NULL);
+
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %lu bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
@@ -65,6 +99,8 @@ void app_main(void)
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 
+    mqtt_app_start();
+
     // // esp_log_level_set(TAG, ESP_LOG_INFO);
     // xTaskCreate(Task_shtc3, "Task_shtc3", 2048, NULL, 2, NULL);
 
@@ -73,6 +109,108 @@ void app_main(void)
     // xTaskCreate(Task_key, "Task_key", 2048, NULL, 3, NULL);
 
     // xTaskCreate(Task_Hlw8032, "Task_Hlw8032", 4096, NULL, 10, NULL);
+}
+
+static void mqtt_app_start(void)
+{
+
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .credentials.username = MQTT_USER_NAME,
+        .credentials.client_id = MQTT_CLIENT_ID,
+        .credentials.authentication.password = MQTT_PASSWORD,
+        .broker.address.hostname = MQTT_HOSTNAME,
+        .broker.address.transport = MQTT_TRANSPORT_OVER_TCP,
+        .broker.address.port = MQTT_PORT,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(MQTT_TAG, "Event dispatched from event loop base=%s, event_id=%lu", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI(MQTT_TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        ESP_LOGI(MQTT_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(MQTT_TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(MQTT_TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(MQTT_TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+        {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(MQTT_TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+    default:
+        ESP_LOGI(MQTT_TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+/*
+{"clientId":"iakajLiNdUB.SmartScoket_01|securemode=2,signmethod=hmacsha256,timestamp=1678600524906|",
+"username":"SmartScoket_01&iakajLiNdUB",
+"mqttHostUrl":"iot-06z00fj6casse3y.mqtt.iothub.aliyuncs.com",
+"passwd":"936d2209acf78e8ebee292674b9c83270c6324a53ff5460e9e8ff9d26fc225a5",
+"port":1883}
+*/
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0)
+    {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
 }
 
 static void event_handler(void *arg, esp_event_base_t event_base,
