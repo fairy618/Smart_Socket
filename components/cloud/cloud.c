115b50ffa98591255ea3f7235bfdb8512b07a3c3
Fairy <2754283833@qq.com>
2023-04-08 19:56:42 +0800

    Update cloud.c

--- components/cloud/cloud.c
+++ components/cloud/cloud.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include "cloud.h"
 
-#include "esp_qcloud_log.h"
-#include "esp_qcloud_console.h"
-#include "esp_qcloud_storage.h"
-#include "esp_qcloud_iothub.h"
-#include "esp_qcloud_prov.h"
-
 void Task_Cloud(void *pvParameters)
 {
     /**
