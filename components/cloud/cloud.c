77e5b574ecb2f58b043ebf33b8adb8a4d8fef0f9
Fairy <2754283833@qq.com>
2023-04-08 19:18:10 +0800

    增加腾讯云组件

---
+++ components/cloud/cloud.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "cloud.h"
+
+#include "esp_qcloud_log.h"
+#include "esp_qcloud_console.h"
+#include "esp_qcloud_storage.h"
+#include "esp_qcloud_iothub.h"
+#include "esp_qcloud_prov.h"
+
---
+++ components/cloud/include/cloud.h
@@ -0,0 +1,4 @@
+#ifndef __CLOUD_H__
+#define __CLOUD_H__
+
+#endif /* __CLOUD_H__ */
