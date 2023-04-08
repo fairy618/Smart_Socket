d40844038563856484371cbdcb2468b422aa056c
Fairy <2754283833@qq.com>
2023-04-08 22:10:27 +0800

    Update cloud.h

--- components/cloud/include/cloud.h
+++ components/cloud/include/cloud.h
@@ -19,12 +19,6 @@
 
 #define REBOOT_UNBROKEN_COUNT_RESET 5
 
-
-
-static esp_err_t get_wifi_config(wifi_config_t *wifi_cfg, uint32_t wait_ms);
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
-static esp_err_t cloud_set_param(const char *id, const esp_qcloud_param_val_t *val);
-static esp_err_t cloud_get_param(const char *id, esp_qcloud_param_val_t *val);
 void Task_Cloud(void *pvParameters);
 
 #endif /* __CLOUD_H__ */
