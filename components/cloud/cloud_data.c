c1b999cacfffd6a950eeef9bdaf59fd0df6a904f
Fairy <2754283833@qq.com>
2023-04-09 12:10:08 +0800

    Update cloud_data.c
    用队列邮箱的当时发送数据

--- components/cloud/cloud_data.c
+++ components/cloud/cloud_data.c
@@ -3,5 +3,17 @@
 
 float sensor_get_env_temp(void)
 {
-    return 23.45;
+    UserData_t UserData;
+    if (xQueueReceive(MailBox, &UserData, portMAX_DELAY) != pdPASS)
+    {
+        ESP_LOGE("qcloud", " --- sensor_get_env_temp falied --- ");
+        return 0;
+    }
+    else
+    {
+        ESP_LOGI("qcloud", " --- sensor_get_env_temp done --- %.2f ", UserData.EnvData.EnvironmentTemperature);
+        return UserData.EnvData.EnvironmentTemperature;
+    }
+
+    return -1;
 }
